659030a16572e3e63d1d3e6de86d0d367eb4ba75
Tasneem92 <32430119+Tasneem92@users.noreply.github.com>
2017-10-26 14:22:10 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-
+#some changes
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')), # anything that's not admin
